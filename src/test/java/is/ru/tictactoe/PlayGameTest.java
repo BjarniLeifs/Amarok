92458debfc60fec5cdfc4965e8e4c0fda88b27e1
dovydas55 <dovydas55@hotmail.com>
2014-10-29 03:40:01 +0000

    test number 2 complete

--- src/test/java/is/ru/tictactoe/PlayGameTest.java
+++ src/test/java/is/ru/tictactoe/PlayGameTest.java
@@ -2,8 +2,11 @@ package is.ru.tictactoe;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import java.util.*;
 
 public class PlayGameTest {
+
+    private Random rand = new Random();
 
     @Test
     public void testInitialiazeTheBoard() {
@@ -27,14 +30,53 @@ public class PlayGameTest {
     }
 
     @Test
-    public void testCheckForInvalidValidMove(){
+    public void testCheckForInvalidMove(){
 	
 	String[][] testBoard = new String [3][3];
 	testBoard = PlayGame.initialiazeTheBoard(testBoard);
-
+	
+	testBoard[0][0] = "X";
+	testBoard[0][2] = "X";
+	testBoard[1][1] = "O";
+	testBoard[1][2] = "O";
+	testBoard[2][1] = "O";
+	
 	Point point = new Point(0, 0);
 	boolean result = PlayGame.checkForInvalidMove(testBoard, point);
-	assertEquals(false, result); 
+	assertEquals(true, result);
+ 
+	point = new Point(0, 1);
+	result = PlayGame.checkForInvalidMove(testBoard, point);
+	assertEquals(false, result);
+
+	point = new Point(0, 2);
+	result = PlayGame.checkForInvalidMove(testBoard, point);
+	assertEquals(true, result);
+
+	point = new Point(1, 0);
+	result = PlayGame.checkForInvalidMove(testBoard, point);
+	assertEquals(false, result);
+
+	point = new Point(1, 1);
+	result = PlayGame.checkForInvalidMove(testBoard, point);
+	assertEquals(true, result);
+	
+	point = new Point(1, 2);
+	result = PlayGame.checkForInvalidMove(testBoard, point);
+	assertEquals(true, result);
+
+	point = new Point(2, 0);
+	result = PlayGame.checkForInvalidMove(testBoard, point);
+	assertEquals(false, result);
+
+	point = new Point(2, 1);
+	result = PlayGame.checkForInvalidMove(testBoard, point);
+	assertEquals(true, result);
+
+	point = new Point(2, 2);
+	result = PlayGame.checkForInvalidMove(testBoard, point);
+	assertEquals(false, result);
+
     }
        
    
