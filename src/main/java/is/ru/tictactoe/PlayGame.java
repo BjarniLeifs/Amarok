af0e8ddf42c51943494710055bbe64a9f9a2d78d
dovydas55 <dovydas55@hotmail.com>
2014-10-29 02:09:55 +0000

    fixes the PlayGame class by dividing the class in many functions

--- src/main/java/is/ru/tictactoe/PlayGame.java
+++ src/main/java/is/ru/tictactoe/PlayGame.java
@@ -7,7 +7,7 @@ import edu.princeton.cs.introcs.Out;
 
 public class PlayGame {
 
-    private static String[][] board = new String [3][3];
+    //private static String[][] board = new String [3][3];
     private final static int boardSize = 9;
     //from algs4
     private static In in = new In();     
@@ -27,36 +27,19 @@ public class PlayGame {
 
 	printWelcome();
 	//----------------
-	String username;
-	out.println(); 
-	out.print("Username: "); username = in.readString();
-	out.println();
-	human_A.setUsername(username);
+	String username = getUsernameForPlayer1(human_A);
 	//----------------
 	printMenu();
 
 	String choice;
 	int tmp = 1;
 	do{
-	    initialiazeTheBoard();
-	    out.print("Your choice: "); choice = in.readString();
-	    out.println();
-	    
-	    if(!Arrays.asList("1", "2", "3", "4",  "0").contains(choice)){
-		while(!Arrays.asList("1", "2", "3", "4", "0").contains(choice))
-		    {
-			out.println("Illegal choice, please try again.");
-			choice = in.readString();
-		    }
-	    }
-
-
+	    //initialiazeTheBoard();
+	        
+	    choice = selectGame();
 	    if(choice.equals("1")|| choice.equals("2") ||choice.equals("3")) out.print(referenceBoardToString());
-	    
-	    if(choice.equals("0")){
-		printInfo();
-	    }
-
+	    if(choice.equals("0")) printInfo();
+	        
 	    //set the game mode
 	    else if(choice.equals("1")){
 		if(tmp == 1){
@@ -67,26 +50,38 @@ public class PlayGame {
 		}
 		play(human_A, human_B);
 	    }
-	    
-	    else if(choice.equals("2")){
-		play(human_A, comp_A);
-	    }
-	    
-	    else if(choice.equals("3")){
-		play(comp_A, comp_B); 
-	    }
-
-	    else if(choice.equals("4")){
-		break;
-	    }
-
-	   
-
-		}while(choice.equals("1")|| choice.equals("2") ||choice.equals("3") || choice.equals("0")); //TODO: write a test for testing if correct game mode is entered after in each case
-
-	//Results table:
+	        
+	    else if(choice.equals("2")) play(human_A, comp_A);
+	    else if(choice.equals("3")) play(comp_A, comp_B); 
+	    else if(choice.equals("4")) break;
+	        
+	}while(choice.equals("1")|| choice.equals("2") ||choice.equals("3") || choice.equals("0")); 
 	printResults(human_A, human_B, comp_A, comp_B); 
 
+    }
+    
+    public static String getUsernameForPlayer1(HumanPlayer human_A){
+	String username;
+	out.println(); 
+	out.print("Username: "); username = in.readString();
+	out.println();
+	human_A.setUsername(username);
+	return username; 
+    }
+    
+    public static String selectGame(){
+	out.print("Your choice: "); String choice = in.readString();
+	out.println();
+
+	if(!Arrays.asList("1", "2", "3", "4",  "0").contains(choice)){
+	    while(!Arrays.asList("1", "2", "3", "4", "0").contains(choice)){
+		out.println("Illegal choice, please try again.");
+		out.print("Your choice: "); choice = in.readString();
+		out.println();
+	    }
+	}
+	
+	return choice;
     }
     
     //printing functions
@@ -98,7 +93,7 @@ public class PlayGame {
 	if(humanB.getWins() > 0 || humanB.getDraw() > 0) out.println("    " + humanB.getUsername() + " won: " + humanB.getWins() + " and tied: " + humanB.getDraw() +" times"); 
 	if(compA.getWins() > 0 || compA.getDraw() > 0) out.println("    " + compA.getUsername() + " won: " + compA.getWins() + " and tied: " + compA.getDraw() +" times"); 
 	if(compB.getWins() > 0 || compB.getDraw() > 0) out.println("    " + compB.getUsername() + " won: " + compB.getWins() + " and tied: " + compB.getDraw() +" times"); 
-	
+	out.println("--------------------------------------------\n");
     }
 
     public static void printInfo(){
@@ -128,7 +123,7 @@ public class PlayGame {
     }
 
     public static void printMenu(){
-	out.println("--------------------------------------------"); 	
+	out.println("--------------------------------------------"); 
 	out.println("\t\t MAIN MENU ");
 	out.println("--------------------------------------------\n"); 
 	out.println("[ Game mode ]");
@@ -155,7 +150,7 @@ public class PlayGame {
 	return sb.toString(); 
     }
 
-    public static String drawUpdatedBoard(){
+    public static String drawUpdatedBoard(String[][] board){
 	StringBuilder sb = new StringBuilder(); 
 	
 	sb.append("  " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + "\t\t|\t     " + " a1 | a2 | a3\n");
@@ -167,100 +162,118 @@ public class PlayGame {
 	return sb.append("\n").toString(); 
     }
 
-    //-----Game logic starts here-----
 
-    //TODO: write a unit test! Check if the board is empty when new game starts
-    //ALSO test, if this function correctly initializes the board. Hence every elment has to be equal to " "
-    public static void initialiazeTheBoard(){
+    //-----Game logic starts here-----
+    
+    public static String[][] initialiazeTheBoard(String[][] board){
 	for(int i = 0; i < 3; i++)
 	    for(int j = 0; j < 3; j++)
 		board[i][j] = " ";
+	return board; 
     }
 
-    
     //play the game...
     public static void play(Player player1, Player player2){
 	Point point;
 	boolean win = false;
-	int finishGame = 0;  
+	int finishGame = 0;
+	String symbol = "X";
+	Player player;
+	String[][] board = new String [3][3];
+	board = initialiazeTheBoard(board);
 	while(!win){
-	    out.println(player1.getUsername()+ "'s " + "time to play "); 
-	    point = player1.nextMove();
-	    if(board[point.getX()][point.getY()] != " "){
-		while(board[point.getX()][point.getY()] != " ") {
-		    out.print("Invalid coordinate! Try again ");
-		    out.println(); 
-		    point = player1.nextMove();
-		}
-	    }
-	    out.println();
-	    board[point.getX()][point.getY()] = "X";
-	    out.print(drawUpdatedBoard());
-	    finishGame++; //x made a move
-	    win = checkWins(); 
-	    if(win){
-		player1.setWins(player1.getWins() + 1);
-		out.println("--------------------------------------------"); 	
-		out.println("\t\t~~~Victory~~~");
-		out.println("--------------------------------------------"); 	
-		out.println("\t\t" + player1.getUsername() + " won!");
-		
-		out.println("\tWould you like to play again!?\n");
-		printMenu();
-		return;
+	    if(finishGame % 2 != 0) { //player2 makes a move
+		player = player2;
+		symbol = "O";
+	    }else { // player 1 makes a move
+		symbol = "X";
+		player = player1; 
 	    }
 	    
+	    point = getApointFromThePlayer(player);
+	    if(checkForValidMove(board, point)){
+		point = makeAvalidMove(board, point, player); 
+	    }
+	    SetAsymbolOnTheBoard(board, point, symbol);
+	    finishGame++; //player1 has made a move
+	            
+	    win = checkWins(board);
+	    if(win){
+		setVictoryForSpecificPlayer(player);
+		return;
+	    }
+	        
 	    // check if draw
-	    if(finishGame >= boardSize){
-		out.println("--------------------------------------------"); 	
-		out.println("\t\t~~~DRAW~~~");
-		out.println("--------------------------------------------");
-		out.println("\tWould you like to play again!?\n");
-		player1.setDraw(player1.getDraw() + 1);
-		player2.setDraw(player2.getDraw() + 1);
-		return; 
-	    }
-	    
-	    // player 2 starts playing  
-	    out.println(player2.getUsername()+ "'s " + "time to play \n");
-	    point = player2.nextMove();
-	    if(board[point.getX()][point.getY()] != " "){
-		while(board[point.getX()][point.getY()] != " ") {
-		    out.print("Invalid coordinate! Try again ");
-		    out.println();
-		    point = player2.nextMove();
-		}
-	    }
-	    board[point.getX()][point.getY()] = "O";
-	    out.print(drawUpdatedBoard());
-	    
-	    finishGame++; //O made a move
-	    win = checkWins(); 
-	    if(win){
-		player2.setWins(player2.getWins() + 1);
-		out.println("--------------------------------------------"); 	
-		out.println("\t\t~~~Victory~~~");
-		out.println("--------------------------------------------"); 	
-		out.println("\t\t" + player2.getUsername() + " won!");
-		
-		out.println("\tWould you like to play again!?\n");
-		printMenu();
+	    if(checkDraw(finishGame, player1, player2)){
 		return;
-	    }
-	    
+	    }      
 	}
-	
     }
 
+    
+    public static boolean checkForValidMove(String[][] board, Point point){
+	if(board[point.getX()][point.getY()] != " "){
+	    return true; 
+	}
+	
+	return false;
+    }
+    
+    public static Point makeAvalidMove(String[][] board, Point point, Player player){
+	while(board[point.getX()][point.getY()] != " ") {
+	    out.print("Invalid coordinate! Try again ");
+	    out.println(); 
+	    point = player.nextMove();
+	}
+	
+	return point;
+    }
+    
+    public static void SetAsymbolOnTheBoard(String[][] board, Point point, String symbol){
+	out.println();
+	board[point.getX()][point.getY()] = symbol;
+	out.print(drawUpdatedBoard(board));
+    }
+    
+    public static boolean checkDraw(int finishGame, Player player1, Player player2){
+	if(finishGame >= boardSize){
+	    out.println("--------------------------------------------"); 
+	    out.println("\t\t~~~DRAW~~~");
+	    out.println("--------------------------------------------");
+	    out.println("\tWould you like to play again!?\n");
+	    player1.setDraw(player1.getDraw() + 1);
+	    player2.setDraw(player2.getDraw() + 1); 
+	    return true;
+	}
+	
+	return false;
+    }
+    
+    public static Point getApointFromThePlayer(Player player){
+	out.println(player.getUsername()+ "'s " + "time to play "); 
+	return player.nextMove();
+    }
+    
+    public static void setVictoryForSpecificPlayer(Player player){
+	player.setWins(player.getWins() + 1);
+	out.println("--------------------------------------------"); 
+	out.println("\t\t~~~Victory~~~");
+	out.println("--------------------------------------------"); 
+	out.println("\t\t" + player.getUsername() + " won!");
+	
+	out.println("\tWould you like to play again!?\n");
+	printMenu();
+    }
+    
     //Winning logic starts here -> TODO: test everything
     //IF ANYONE SEES HOW TO MAKE THIS PART SHORTER, GO AHEAD AND CHANGE IT!
 
-    public static boolean checkWins(){ 
-	if(checkHorizontalforX() || checkHorizontalforO() || checkVerticalforX() || checkVerticalforO() || checkDiagnal() || chechInvDiagnal()) return true;
+    public static boolean checkWins(String[][] board){ 
+	if(checkHorizontalforX(board) || checkHorizontalforO(board) || checkVerticalforX(board) || checkVerticalforO(board) || checkDiagnal(board) || chechInvDiagnal(board)) return true;
 	return false; 
     }
     
-    private static boolean checkHorizontalforX(){
+    private static boolean checkHorizontalforX(String[][] board){
 	boolean h1 = true;
 	boolean h2 = true;
 	boolean h3 = true;
@@ -281,7 +294,7 @@ public class PlayGame {
 	return false; 
     }
     
-    private static boolean checkHorizontalforO(){
+    private static boolean checkHorizontalforO(String[][] board){
 	boolean h1 = true;
 	boolean h2 = true;
 	boolean h3 = true;
@@ -302,7 +315,7 @@ public class PlayGame {
 	return false; 
     }
     
-    private static boolean checkVerticalforX(){
+    private static boolean checkVerticalforX(String[][] board){
 	boolean v1 = true;
 	boolean v2 = true;
 	boolean v3 = true;
@@ -323,7 +336,7 @@ public class PlayGame {
 	return false; 
     }
     
-    private static boolean checkVerticalforO(){
+    private static boolean checkVerticalforO(String[][] board){
 	boolean v1 = true;
 	boolean v2 = true;
 	boolean v3 = true;
@@ -343,20 +356,19 @@ public class PlayGame {
 	if(v1 || v2 || v3) return true;
 	return false; 
     }
-    
-    private static boolean checkDiagnal(){
+        
+    private static boolean checkDiagnal(String[][] board){
 	if(board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") return true; 
 	else if(board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O") return true; 
 	return false;
     }
     
-    private static boolean chechInvDiagnal(){
+    private static boolean chechInvDiagnal(String[][] board){
 	if(board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") return true; 
 	else if(board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O") return true; 
 	return false;
     }
 
 
-    
-
 }
+
